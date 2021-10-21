1b07bc582a9e137864c93b0deffed6ec5a0e6f61
scorreah <scorreah@eafit.edu.co>
2021-10-21 11:22:25 -0500

    Configuración de correo

--- MiUniversidad/MiUniversidad/settings.py
+++ MiUniversidad/MiUniversidad/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USER_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "correo@gmail.com"
+EMAIL_HOST_PASSWORD = "clave del correo"
