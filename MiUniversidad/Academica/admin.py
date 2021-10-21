159da30b9e8b3824d74e6c9ffa772b78fb9b86ef
scorreah <scorreah@eafit.edu.co>
2021-10-21 10:19:03 -0500

    Registro de modelos en admin

--- MiUniversidad/Academica/admin.py
+++ MiUniversidad/Academica/admin.py
@@ -1,3 +1,11 @@
+# Django
 from django.contrib import admin
 
+# Models
+from Academica.models import *
+
 # Register your models here.
+admin.site.register(Carrera)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+admin.site.register(Matricula)
