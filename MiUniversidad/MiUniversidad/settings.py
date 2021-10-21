16d7ce3ad2e480eef0d8af94616340820860300e
scorreah <scorreah@eafit.edu.co>
2021-10-21 12:13:59 -0500

    Funcionalidad Envio de correos

--- MiUniversidad/MiUniversidad/settings.py
+++ MiUniversidad/MiUniversidad/settings.py
@@ -129,8 +129,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USER_TLS = True
+
+EMAIL_USE_TLS = True
+
 EMAIL_PORT = 587
+
 EMAIL_HOST_USER = "correo@gmail.com"
-EMAIL_HOST_PASSWORD = "clave del correo"
+
+EMAIL_HOST_PASSWORD = "contraseña"
--- MiUniversidad/MiUniversidad/urls.py
+++ MiUniversidad/MiUniversidad/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 # Views
-from Academica.views import formularioContacto
+from Academica.views import formularioContacto, contactar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('formularioContacto/', formularioContacto)
+    path('formularioContacto/', formularioContacto),
+    path('contactar/', contactar),
 ]
