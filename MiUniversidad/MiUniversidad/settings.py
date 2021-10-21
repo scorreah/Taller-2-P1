7336efdca69f15b685ef3f9aad29ffe9f92410ed
scorreah <scorreah@eafit.edu.co>
2021-10-21 09:52:42 -0500

    Configuraciones settings.py

--- MiUniversidad/MiUniversidad/settings.py
+++ MiUniversidad/MiUniversidad/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'MiUniversidad.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'MiUniversidad.db',
     }
 }
 
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
